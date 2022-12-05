e4c9e14fc9bc97c5fd428a7990dfa807b39206d6
Tolya Matskanyuk <tolyamatskanyuk@gmail.com>
2022-12-05 14:46:45 +0200

    lab13 commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
 
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
+
+
   # GET /products or /products.json
   def index
-    @products = Product.all
+     @products = Product.all.order(:title)
   end
 
   # GET /products/1 or /products/1.json
@@ -41,6 +52,10 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+
+        @products = Product.all.order(:title)
+        ActionCable.server.broadcast 'products', html: render_to_string('store/index', layout: false)
+
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
